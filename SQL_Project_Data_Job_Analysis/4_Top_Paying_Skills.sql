f96c566a380be83967e093c0add808b28fe4e483
Robin J. St.Pierre <robinjstpierre@gmail.com>
2026-02-12 17:16:50 -0500

    Project

--- SQL_Project_Data_Job_Analysis/4_Top_Paying_Skills.sql
+++ SQL_Project_Data_Job_Analysis/4_Top_Paying_Skills.sql
@@ -3,13 +3,13 @@ Quesiton: What are the top paying data analyst jobs?
 -Identify the top 10 higest paying Data Analyst roles available in the state of Florida. Focus on postings that have specified salaries.
 -What skills are required for higher paying jobs?
 -What are the top 5 in demand skills in the state of Florida?
+-Top Paying skills
 
 */
 
- 
 SELECT
     skills,
-    COUNT(skills_job_dim.job_id) AS demand_count
+    ROUND(AVG(salary_year_avg),0) AS Average_Salary
 FROM job_postings_fact
 INNER JOIN skills_job_dim 
     ON job_postings_fact.job_id = skills_job_dim.job_id
@@ -17,8 +17,10 @@ INNER JOIN skills_dim
     ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL AND
     job_location = 'Florida'
 GROUP BY
     skills
-ORDER BY demand_count DESC
-LIMIT 5;
+ORDER BY
+    Average_Salary DESC
+LIMIT 25;
